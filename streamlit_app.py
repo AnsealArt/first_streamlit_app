52126d2db1c87447b1b0464ef5932afcb11b8b6a
Filip Dzięcioł <48879563+AnsealArt@users.noreply.github.com>
2022-07-20 17:48:14 +0200

    Add a mock for possibility of choosing the fruit and

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,11 +22,14 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
+
 # New Section to display fruityvice API response
 streamlit.header("Fruityvice Fruit Advice!")
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 
 # Take Fruityvice JSON and normalize it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
