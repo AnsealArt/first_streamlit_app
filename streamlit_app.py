ed8e79b4c41de2d4ec1e2aed5ab08b19cb858043
Filip Dzięcioł <48879563+AnsealArt@users.noreply.github.com>
2022-07-22 08:44:56 +0200

    Fix my_cnx indent

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ def get_fruityvice_data(this_fruit_choice):
   return fruityvice_normalized
  
 def get_fruit_load_list():
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   with my_cnx.cursor() as my_cur:
     my_cur.execute("SELECT * FROM fruit_load_list")
     my_cnx.close()
